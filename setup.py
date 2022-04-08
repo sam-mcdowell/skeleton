ef893422679cd15f65d67d5fd816282659c9393e
sam-mcdowell <sam.mcdowell@gmail.com>
2022-04-07 19:15:55 -0700

    rename and simplify

--- setup.py
+++ setup.py
@@ -8,18 +8,13 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
-    name='sample',
+    name='skeleton',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='skeleton module',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
-    license=license,
+    author='nobody',
+    author_email='no@body.com',
     packages=find_packages(exclude=('tests', 'docs'))
 )
 
